5d3239bddba7a200b1ea66f58540dc4e29212cc3
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-18 01:35:27 -0300

    Alteração 1

---
+++ OperadoresRelacionais.java
@@ -0,0 +1,28 @@
+package Fundamentos-de-Java;
+
+// Usados para comparar valores. Ex: ==, !=, >, <, >=, <=
+// Crie um programa que verifique se um número é maior que 10:
+
+
+public class OperadoresRelacionais {
+    public static void main(String [] args){
+
+        int numero = 15;  // --> Definindo uma variável.
+
+        if (numero > 10) {                                             
+            System.out.println("O Número é maior que 10.");
+        } else { 
+            System.out.println("O Número não é maior que 10");
+        }
+
+
+    }
+
+
+}
+
+
+// Legenda:
+
+  // if significa: SE...
+  // Else significa: SE NÃO...
