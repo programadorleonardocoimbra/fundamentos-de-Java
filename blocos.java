0b81a39b2dd8802dcee9c7833db2c15c9bb8d397
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-18 02:14:19 -0300

    Alterações 03

---
+++ blocos.java
@@ -0,0 +1,23 @@
+//Trechos delimitados por { } que organizam o código.
+
+//Exercícios:
+    //Crie um programa que verifica se um número é positivo e imprime mensagens dentro de blocos:
+
+
+public class blocos {
+  public static void main(String[] args) {
+        int numero = -5;
+
+        if (numero > 0) {
+            System.out.println("Número positivo");
+
+            if (numero > 10) {
+                System.out.println("Número maior que 10");
+            }
+        } else {
+            System.out.println("Número negativo ou zero");
+        }
+    }
+}
+    
+
